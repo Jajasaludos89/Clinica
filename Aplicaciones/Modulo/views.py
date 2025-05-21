0fd6a7b5c88a545f1eb6645442e67e351ac77eea
Javier Pilco <brandon.pilco3401@utc.edu.ec>
2025-05-21 14:48:30 -0500

    Creacion del primer views

--- Aplicaciones/Modulo/views.py
+++ Aplicaciones/Modulo/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Empleado, AveDePresa, SesionFisioterapia
+from django.contrib import messages
 
-# Create your views here.
+
+#Empleado
+
+def inicioEmpleados(request):
+    empleados = Empleado.objects.all()
+    return render(request, "inicioEmpleados.html", {'empleados': empleados})
+
+def nuevoEmpleado(request):
+    return render(request, "nuevoEmpleado.html")
+
+def guardarEmpleado(request):
+    identificacion = request.POST["identificacion"]
+    nombre = request.POST["nombre"]
+    edad = request.POST["edad"]
+    titulo = request.POST["titulo"]
+    especialidad = request.POST["especialidad"]
+
+    Empleado.objects.create(
+        identificacion=identificacion,
+        nombre=nombre,
+        edad=edad,
+        titulo=titulo,
+        especialidad=especialidad
+    )
+
+    messages.success(request, "Empleado GUARDADO exitosamente")
+    return redirect('/empleados')
+
+def eliminarEmpleado(request, id):
+    empleado = Empleado.objects.get(id=id)
+    empleado.delete()
+    messages.success(request, "Empleado ELIMINADO exitosamente")
+    return redirect('/empleados')
+
+def editarEmpleado(request, id):
+    empleado = Empleado.objects.get(id=id)
+    return render(request, "editarEmpleado.html", {'empleado': empleado})
+
+def procesarEdicionEmpleado(request):
+    id = request.POST["id"]
+    empleado = Empleado.objects.get(id=id)
+    empleado.identificacion = request.POST["identificacion"]
+    empleado.nombre = request.POST["nombre"]
+    empleado.edad = request.POST["edad"]
+    empleado.titulo = request.POST["titulo"]
+    empleado.especialidad = request.POST["especialidad"]
+    empleado.save()
+    messages.success(request, "Empleado ACTUALIZADO exitosamente")
+    return redirect('/empleados')
